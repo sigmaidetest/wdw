a6c7273a62eb441b24362db6a785aaae46e0c68c
sigmaidetest <cehib@balanc3r.com>
2019-01-18 11:07:24 +0530

    dwd

--- wdw/lambda.js
+++ wdw/lambda.js
@@ -1,20 +1,4 @@
-let AWS = require('aws-sdk');
-let SL_TWITTER = require('slappforge-sdk-twitter');
-let twitterClients = require('./TwitterClients');
-const twitter = new SL_TWITTER.TwitterP(twitterClients);
-
-exports.handler = function (event, context, callback) {
-    twitter.searchTweets({
-        "searchParams": {
-            "q": "vdsd",
-            "count": 100
-        },
-        "clientName": "twClient"
-    }).then(response => {
-        let data = response.data;
-    }).catch(err => {
-        console.log(err);
-    });
-
-    callback(null, 'Successfully executed');
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
